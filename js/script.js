62ff0106362c6f643e50710cd404310b3123784b
moshesfez807.github.io <moshesfez807@gmail.com>
2025-09-14 22:18:31 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 function sayHello() {
-  alert("שלום! הכל עובד 😄");
+  alert("שלום! לחצת על הכפתור 😄");
+}
+
+function sendMessage(event) {
+  event.preventDefault();
+  const name = document.getElementById('name').value;
+  const message = document.getElementById('message').value;
+  if (name && message) {
+    alert(`תודה ${name}, ההודעה שלך נשלחה!`);
+  } else {
+    alert("אנא מלא את כל השדות");
+  }
 }
